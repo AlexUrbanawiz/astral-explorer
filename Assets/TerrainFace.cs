153894868146d26437e36bf40cef064948adf269
AlexUrbanawiz <koalapokemonfun@gmail.com>
2025-09-25 12:28:58 -0600

    Planet Editing Progress:

--- Assets/TerrainFace.cs
+++ Assets/TerrainFace.cs
@@ -7,9 +7,10 @@ public class TerrainFace
     Vector3 localUp;
     Vector3 axisA;
     Vector3 axisB;
-
-    public TerrainFace(Mesh mesh, int resolution, Vector3 localUp)
+    ShapeGenerator shapeGenerator;
+    public TerrainFace(ShapeGenerator shapeGenerator, Mesh mesh, int resolution, Vector3 localUp)
     {
+        this.shapeGenerator = shapeGenerator; 
         this.mesh = mesh;
         this.resolution = resolution;
         this.localUp = localUp;
@@ -32,7 +33,7 @@ public class TerrainFace
                 Vector2 percent = new Vector2(x, y) / (resolution - 1);
                 Vector3 pointOnUnitCube = localUp + (percent.x - .5f) * 2 * axisA + (percent.y - .5f) * 2 * axisB;
                 Vector3 pointOnUnitSphere = pointOnUnitCube.normalized;
-                verticies[i] = pointOnUnitSphere;
+                verticies[i] = shapeGenerator.CalculatePointOnPlanet(pointOnUnitSphere);
 
                 if (x != resolution - 1 && y != resolution - 1)
                 {
