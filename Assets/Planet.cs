ff4b40986205da7b6fa7a593b8f5cc03e97b705a
AlexUrbanawiz <koalapokemonfun@gmail.com>
2025-09-29 15:17:18 -0600

    Multiple Noise Filters

--- Assets/Planet.cs
+++ Assets/Planet.cs
@@ -9,9 +9,9 @@ public class Planet : MonoBehaviour
 
     [Range(2, 256)]
     public int resolution = 10;
-
     public bool autoUpate = true;
-
+    public enum FaceRenderMask {All, Top, Bottom, Left, Right, Front, Back}
+    public FaceRenderMask faceRenderMask;
     public ShapeSettings shapeSettings;
     public ColorSettings colorSettings;
 
@@ -49,6 +49,8 @@ public class Planet : MonoBehaviour
                 meshFilters[i].sharedMesh = new Mesh();
             }
             terrainFaces[i] = new TerrainFace(shapeGenerator, meshFilters[i].sharedMesh, resolution, directions[i]);
+            bool renderFace = faceRenderMask == FaceRenderMask.All || (int)faceRenderMask - 1 == i;
+            meshFilters[i].gameObject.SetActive(renderFace);
         }
     }
 
@@ -79,9 +81,12 @@ public class Planet : MonoBehaviour
 
     void GenerateMesh()
     {
-        foreach (TerrainFace face in terrainFaces)
+        for (int i = 0; i < 6; i++)
         {
-            face.ConstructMesh();
+            if (meshFilters[i].gameObject.activeSelf)
+            {
+                terrainFaces[i].ConstructMesh();
+            }
         }
     }
 
