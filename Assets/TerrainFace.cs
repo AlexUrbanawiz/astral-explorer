7d47f66019b873ddee17596846e082606ec9fdca
AlexUrbanawiz <koalapokemonfun@gmail.com>
2025-09-30 15:00:57 -0600

    Simple Biomes

--- Assets/TerrainFace.cs
+++ Assets/TerrainFace.cs
@@ -10,7 +10,7 @@ public class TerrainFace
     ShapeGenerator shapeGenerator;
     public TerrainFace(ShapeGenerator shapeGenerator, Mesh mesh, int resolution, Vector3 localUp)
     {
-        this.shapeGenerator = shapeGenerator; 
+        this.shapeGenerator = shapeGenerator;
         this.mesh = mesh;
         this.resolution = resolution;
         this.localUp = localUp;
@@ -24,7 +24,7 @@ public class TerrainFace
         Vector3[] verticies = new Vector3[resolution * resolution];
         int[] triangles = new int[((resolution - 1) * (resolution - 1) * 6)];
         int triIndex = 0;
-
+        Vector2[] uv = mesh.uv;
         for (int y = 0; y < resolution; y++)
         {
             for (int x = 0; x < resolution; x++)
@@ -52,5 +52,25 @@ public class TerrainFace
         mesh.vertices = verticies;
         mesh.triangles = triangles;
         mesh.RecalculateNormals();
+        mesh.uv = uv;
+    }
+
+    public void UpdateUVs(ColorGenerator colorGenerator)
+    {
+        Vector2[] uv = new Vector2[resolution * resolution];
+
+        for (int y = 0; y < resolution; y++)
+        {
+            for (int x = 0; x < resolution; x++)
+            {
+                int i = x + y * resolution;
+                Vector2 percent = new Vector2(x, y) / (resolution - 1);
+                Vector3 pointOnUnitCube = localUp + (percent.x - .5f) * 2 * axisA + (percent.y - .5f) * 2 * axisB;
+                Vector3 pointOnUnitSphere = pointOnUnitCube.normalized;
+
+                uv[i] = new Vector2(colorGenerator.BiomePercentFromPoint(pointOnUnitSphere), 0);
+            }
+        }
+        mesh.uv = uv;
     }
 }
